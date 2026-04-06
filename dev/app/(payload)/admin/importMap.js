f4804a6917e24cfe7e9d515e84760d921e94006c
Kfir <kfir25812@gmail.com>
2026-04-06 09:39:26 +0300

    feat: initial commit

---
+++ dev/app/(payload)/admin/importMap.js
@@ -0,0 +1,9 @@
+import { BeforeDashboardClient as BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343 } from 'payload-storage-file-explorer/client'
+import { BeforeDashboardServer as BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f } from 'payload-storage-file-explorer/rsc'
+
+export const importMap = {
+  'payload-storage-file-explorer/client#BeforeDashboardClient':
+    BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343,
+  'payload-storage-file-explorer/rsc#BeforeDashboardServer':
+    BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f,
+}
